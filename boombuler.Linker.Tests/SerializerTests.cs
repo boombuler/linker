357d85d1e30476e78aabb65ae3fa4cd0d8c312d4
boombuler <boombuler@gmail.com>
2025-05-13 05:31:44 +0200

    Started implementing a Module Serializer

---
+++ boombuler.Linker.Tests/SerializerTests.cs
@@ -0,0 +1,50 @@
+﻿namespace boombuler.Linker.Tests;
+
+using System.Numerics;
+using boombuler.Linker.Module;
+
+[TestClass]
+public class SerializerTests
+{
+    private static byte[] Serialize<TAddr>(Module<TAddr> module)
+        where TAddr : struct, IUnsignedNumber<TAddr>, INumberBase<TAddr>
+    {
+        using var stream = new MemoryStream();
+        var serializer = new ModuleSerializer();
+        serializer.Serialize(module, stream);
+        return stream.ToArray();
+    }
+
+    [TestMethod]
+    public void Modules_Headers__Contain_the_Address_Length()
+    {
+        var mod = Serialize(new Module<byte>() { Name = "Test" });
+        CollectionAssert.AreEqual(new byte[] {
+            0x42, 0x4C, 0x4B, 0x01,       // Magic Number + Address Length
+            0x04, 0x54, 0x65, 0x73, 0x74, // Module Name
+            0x00, 0x00                    // Symbol Count
+        }, mod);
+
+        mod = Serialize(new Module<ushort>() { Name = "Test" });
+        CollectionAssert.AreEqual(new byte[] { 
+            0x42, 0x4C, 0x4B, 0x02,       // Magic Number + Address Length
+            0x04, 0x54, 0x65, 0x73, 0x74, // Module Name
+            0x00, 0x00                    // Symbol Count
+        }, mod);
+
+        
+        mod = Serialize(new Module<uint>() { Name = "Test" });
+        CollectionAssert.AreEqual(new byte[] {
+            0x42, 0x4C, 0x4B, 0x04,       // Magic Number + Address Length
+            0x04, 0x54, 0x65, 0x73, 0x74, // Module Name
+            0x00, 0x00                    // Symbol Count
+        }, mod);
+
+        mod = Serialize(new Module<ulong>() { Name = "Test" });
+        CollectionAssert.AreEqual(new byte[] {
+            0x42, 0x4C, 0x4B, 0x08,       // Magic Number + Address Length
+            0x04, 0x54, 0x65, 0x73, 0x74, // Module Name
+            0x00, 0x00                    // Symbol Count
+        }, mod);
+    }
+}
---
+++ boombuler.Linker/Module/ModuleSerializer.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace boombuler.Linker.Module;
+
+public class ModuleSerializer
+{
+    private const string MagicNumber = "BLK";
+    public void Serialize<TAddr>(Module<TAddr> module, Stream target)
+        where TAddr : struct, IUnsignedNumber<TAddr>, INumberBase<TAddr>
+    {
+        ArgumentNullException.ThrowIfNull(module);
+        ArgumentNullException.ThrowIfNull(target);
+
+        using var writer = new BinaryWriter(target, Encoding.UTF8);
+
+        writer.Write(Encoding.UTF8.GetBytes(MagicNumber));
+        writer.Write((byte)Marshal.SizeOf<TAddr>());
+        writer.Write(module.Name);
+        writer.Write((ushort)module.Symbols.Length);
+        foreach (var symbol in module.Symbols)
+        {
+            writer.Write((byte)symbol.Type);
+            writer.Write(symbol.Name.Global ?? string.Empty);
+            writer.Write(symbol.Name.Local ?? string.Empty);
+        }
+    }
+}
