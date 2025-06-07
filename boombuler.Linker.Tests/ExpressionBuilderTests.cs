6859699d71a0732a60834ed3d5955355700364aa
boombuler <boombuler@gmail.com>
2025-06-07 07:55:18 +0200

    Added Expression Build
    This reduces constant expression collections by calculating constant expressions if possible.

---
+++ boombuler.Linker.Tests/ExpressionBuilderTests.cs
@@ -0,0 +1,171 @@
+﻿namespace boombuler.Linker.Tests;
+
+using boombuler.Linker.Patches;
+using static boombuler.Linker.Patches.Expression;
+
+[TestClass]
+public class ExpressionBuilderTests
+{
+    [TestMethod]
+    public void Add_Expressions__Are_Reduced()
+    {
+        var result = new ExpressionBuilder()
+            .Add(Push(1))
+            .Add(Push(2))
+            .Add(Add)
+            .Build();
+
+        Assert.AreEqual(1, result.Count);
+        Assert.IsTrue(result.First() is { Type: ExpressionType.Push, Value: 3 });
+    }
+
+    [TestMethod]
+    public void Sub_Expressions__Are_Reduced()
+    {
+        var result = new ExpressionBuilder()
+            .Add(Push(1))
+            .Add(Push(2))
+            .Add(Sub)
+            .Build();
+
+        Assert.AreEqual(1, result.Count);
+        Assert.IsTrue(result.First() is { Type: ExpressionType.Push, Value: 0xFFFFFFFF_FFFFFFFF });
+    }
+
+    [TestMethod]
+    public void Mul_Expressions__Are_Reduced()
+    {
+        var result = new ExpressionBuilder()
+            .Add(Push(3))
+            .Add(Push(2))
+            .Add(Mul)
+            .Build();
+
+        Assert.AreEqual(1, result.Count);
+        Assert.IsTrue(result.First() is { Type: ExpressionType.Push, Value: 6 });
+    }
+
+    [TestMethod]
+    public void And_Expressions__Are_Reduced()
+    {
+        var result = new ExpressionBuilder()
+            .Add(Push(0b110011))
+            .Add(Push(0b101010))
+            .Add(And)
+            .Build();
+
+        Assert.AreEqual(1, result.Count);
+        Assert.IsTrue(result.First() is { Type: ExpressionType.Push, Value: 0b100010 });
+    }
+
+    [TestMethod]
+    public void Or_Expressions__Are_Reduced()
+    {
+        var result = new ExpressionBuilder()
+            .Add(Push(0b110011))
+            .Add(Push(0b101010))
+            .Add(Or)
+            .Build();
+
+        Assert.AreEqual(1, result.Count);
+        Assert.IsTrue(result.First() is { Type: ExpressionType.Push, Value: 0b111011 });
+    }
+
+    [TestMethod]
+    public void Xor_Expressions__Are_Reduced()
+    {
+        var result = new ExpressionBuilder()
+            .Add(Push(0b110011))
+            .Add(Push(0b101010))
+            .Add(Xor)
+            .Build();
+
+        Assert.AreEqual(1, result.Count);
+        Assert.IsTrue(result.First() is { Type: ExpressionType.Push, Value: 0b011001 });
+    }
+
+    [TestMethod]
+    public void Shl_Expressions__Are_Reduced()
+    {
+        var result = new ExpressionBuilder()
+            .Add(Push(0b110011))
+            .Add(Push(2))
+            .Add(Shl)
+            .Build();
+
+        Assert.AreEqual(1, result.Count);
+        Assert.IsTrue(result.First() is { Type: ExpressionType.Push, Value: 0b11001100 });
+    }
+
+    [TestMethod]
+    public void Shr_Expressions__Are_Reduced()
+    {
+        var result = new ExpressionBuilder()
+            .Add(Push(0b110011))
+            .Add(Push(2))
+            .Add(Shr)
+            .Build();
+
+        Assert.AreEqual(1, result.Count);
+        Assert.IsTrue(result.First() is { Type: ExpressionType.Push, Value: 0b1100 });
+    }
+
+    [TestMethod]
+    public void Cpl_Expressions__Are_Reduced()
+    {
+        var result = new ExpressionBuilder()
+            .Add(Push(0x123456789ABCDEF0))
+            .Add(Cpl)
+            .Build();
+
+        Assert.AreEqual(1, result.Count);
+        Assert.IsTrue(result.First() is { Type: ExpressionType.Push, Value: 0xEDCBA9876543210F });
+    }
+
+    [TestMethod]
+    public void AddRange_will_reduce_all_expressions()
+    {
+        var result = new ExpressionBuilder()
+            .AddRange(
+            [
+                Push(1),
+                Push(2),
+                Add,
+                Push(3),
+                Push(4),
+                Add,
+                Add
+            ])
+            .Build();
+        Assert.AreEqual(1, result.Count);
+        Assert.IsTrue(result.First() is { Type: ExpressionType.Push, Value: 10 });
+    }
+
+    [TestMethod]
+    public void Invalid_Binary_Expressions__Are_not_reduced()
+    {
+        var result = new ExpressionBuilder()
+            .Add(Add)
+            .Build();
+        Assert.AreEqual(1, result.Count);
+        Assert.IsTrue(result.First() is { Type: ExpressionType.Add });
+
+        result = new ExpressionBuilder()
+            .Add(Push(1))
+            .Add(Add)
+            .Build();
+        Assert.AreEqual(2, result.Count);
+        Assert.IsTrue(result.First() is { Type: ExpressionType.Push, Value: 1 });
+        Assert.IsTrue(result.Last() is { Type: ExpressionType.Add });
+    }
+
+    [TestMethod]
+    public void Invalid_Unary_Expressions__Are_not_reduced()
+    {
+        var result = new ExpressionBuilder()
+            .Add(Cpl)
+            .Build();
+        Assert.AreEqual(1, result.Count);
+        Assert.IsTrue(result.First() is { Type: ExpressionType.Cpl });
+    }
+}
---
+++ boombuler.Linker/Patches/ExpressionBuilder.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boombuler.Linker.Patches
+{
+    /// <summary>
+    /// Helper class to build a list of expressions, optimizing constant calculations
+    /// while adding Expressions.
+    /// </summary>
+    public class ExpressionBuilder
+    {
+        private readonly List<Expression> fExpressions = new();
+        
+        public ExpressionBuilder Add(Expression expression)
+        {
+            switch (expression.Type)
+            {
+                case ExpressionType.Add when TryGetArgs(out var a, out var b):
+                    fExpressions.RemoveAt(fExpressions.Count - 1);
+                    fExpressions[^1] = Expression.Push(a + b);
+                    break;
+                case ExpressionType.Sub when TryGetArgs(out var a, out var b):
+                    fExpressions.RemoveAt(fExpressions.Count - 1);
+                    fExpressions[^1] = Expression.Push(a - b);
+                    break;
+                case ExpressionType.Mul when TryGetArgs(out var a, out var b):
+                    fExpressions.RemoveAt(fExpressions.Count - 1);
+                    fExpressions[^1] = Expression.Push(a * b);
+                    break;
+                case ExpressionType.And when TryGetArgs(out var a, out var b):
+                    fExpressions.RemoveAt(fExpressions.Count - 1);
+                    fExpressions[^1] = Expression.Push(a & b);
+                    break;
+                case ExpressionType.Or when TryGetArgs(out var a, out var b):
+                    fExpressions.RemoveAt(fExpressions.Count - 1);
+                    fExpressions[^1] = Expression.Push(a | b);
+                    break;
+                case ExpressionType.Xor when TryGetArgs(out var a, out var b):
+                    fExpressions.RemoveAt(fExpressions.Count - 1);
+                    fExpressions[^1] = Expression.Push(a ^ b);
+                    break;
+                case ExpressionType.Shl when TryGetArgs(out var a, out var b):
+                    fExpressions.RemoveAt(fExpressions.Count - 1);
+                    fExpressions[^1] = Expression.Push(a << (int)b);
+                    break;
+                case ExpressionType.Shr when TryGetArgs(out var a, out var b):
+                    fExpressions.RemoveAt(fExpressions.Count - 1);
+                    fExpressions[^1] = Expression.Push(a >> (int)b);
+                    break;
+                case ExpressionType.Cpl when TryGetArgs(out var arg):
+                    fExpressions[^1] = Expression.Push(~arg);
+                    break;
+                default:
+                    fExpressions.Add(expression);
+                    break;
+            }
+            return this;
+        }
+
+        public ExpressionBuilder AddRange(IEnumerable<Expression> expressions)
+        {
+            foreach (var ex in expressions)
+                Add(ex);
+            return this;
+        }
+
+        private bool TryGetArgs(out ulong arg)
+        {
+            if (fExpressions.Count > 0 && fExpressions[^1] is { Type: ExpressionType.Push, Value: var value })
+            {
+                arg = value;
+                return true;
+            }
+            arg = default;
+            return false;
+        }
+
+        private bool TryGetArgs(out ulong a, out ulong b)
+        {
+            if (fExpressions.Count > 1 && 
+                fExpressions[^1] is { Type: ExpressionType.Push, Value: var valueB } &&
+                fExpressions[^2] is { Type: ExpressionType.Push, Value: var valueA })
+            {
+                a = valueA;
+                b = valueB;
+                return true;
+            }
+            a = b = default;
+            return false;
+        }
+
+        public IReadOnlyCollection<Expression> Build()
+        {
+            if (fExpressions.Count == 0)
+                return Array.Empty<Expression>();
+            return fExpressions.ToArray();
+        }
+    }
+}
